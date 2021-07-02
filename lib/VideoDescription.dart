6ded3ad9c725f36f18dbbf96ef429504c765e6c6
Antoine Labard <antoine@labard.fr>
2021-07-02 18:23:36 +0200

    Implemented VideoDescription.dart

--- lib/VideoDescription.dart
+++ lib/VideoDescription.dart
@@ -4,16 +4,31 @@ class VideoDescription extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
+        child: Container(
+      height: 70,
+      padding: EdgeInsets.only(left: 20),
       child: Column(
-        mainAxisSize: MainAxisSize.min,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: List<Widget>.generate(
-            5,
-            (index) => Container(
-                height: 10,
-                color: Colors.green,
-                margin: EdgeInsets.only(top: 5))),
-      ),
-    );
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '@antoinelabard',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Text('Video title'),
+            Row(
+              children: [
+                Icon(
+                  Icons.music_note,
+                  size: 15,
+                ),
+                Text(
+                  'Artist name - Album name - song',
+                  style: TextStyle(fontSize: 12),
+                )
+              ],
+            )
+          ]),
+    ));
   }
 }
